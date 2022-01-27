b1850d6c0d58b40b6eefe2ffe3d5233d576346dd
Telmiyo <telmoberoiz@hotmail.com>
2022-01-27 17:16:54 +0100

    WishList Class Created

---
+++ burgir_app/lib/burger.dart
@@ -0,0 +1,13 @@
+class Burger {
+  final String name;
+  // ignore: prefer_typing_uninitialized_variables
+  final img;
+  final int kcal;
+  final bool wishlist;
+  Burger(
+    this.name,
+    this.kcal,
+    this.img,
+    this.wishlist,
+  );
+}
